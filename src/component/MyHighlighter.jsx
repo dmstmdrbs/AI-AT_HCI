d17b30df71e4ea18a95c1473011049dc92c063fd
dmstmdrbs <dmstmdrbs98@gmail.com>
2021-08-02 01:50:24 +0900

    Modify text style

--- src/component/MyHighlighter.jsx
+++ src/component/MyHighlighter.jsx
@@ -21,16 +21,16 @@ const MyHiglighter = ({ sentence, weight }) => {
     console.log(weight);
     const markSentence = () => {
       return (
-        <span>
+        <>
           {words.map((word, idx) => {
             if (weight[idx] > 0.07) return <RedSpan>{word} </RedSpan>;
-            if (weight[idx] > 0.06) return <BlueSpan>{word} </BlueSpan>;
-            if (weight[idx] > 0.05) return <YellowSpan>{word} </YellowSpan>;
+            if (weight[idx] > 0.02) return <BlueSpan>{word} </BlueSpan>;
+            if (weight[idx] > 0.01) return <YellowSpan>{word} </YellowSpan>;
             else {
               return <span>{word} </span>;
             }
           })}
-        </span>
+        </>
       );
     };
     setMarked(markSentence());
